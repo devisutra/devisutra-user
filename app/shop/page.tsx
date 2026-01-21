3d469bfa658fb4e56af0f1c3c21f480e1504de42
Rohit Codess <rohitmahto1122@gmail.com>
2026-01-21 12:05:24 +0530

    Fix ProductCard props in shop page

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -69,7 +69,14 @@ export default function ShopPage() {
       ) : (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
           {products.map((product) => (
-            <ProductCard key={product._id} product={product} onAddToCart={handleAddToCart} />
+            <ProductCard 
+              key={product._id} 
+              id={product._id} 
+              title={product.title}
+              price={product.price}
+              category={product.category}
+              image={product.images[0]}
+            />
           ))}
         </div>
       )}
